1d95d54965e77a338da83776be4daa9027600c9d
Krzysztof Napiórkowski <krzysna@gmail.cin>
2023-05-19 21:52:27 +0200

    minor fix

--- PolynominalRegression.py
+++ PolynominalRegression.py
@@ -5,35 +5,48 @@ Created on Wed May 17 17:03:39 2023
 
 @author: krzysiu
 """
+import numpy as np
 
 class PolynominalRegression:
-    __X
-    __y
-    def __init__(self):
+    def __init__(self, degree=2):
+        self.__omega  = None
+        self.__degree = degree # TODO
+    
+    def fit(self, x, y):
+        """Train model"""
+        self.__standard(x, y)
         
-    def __hypothesis(X, theta):
-        y = theta*X
-        return np.sum(y, axis=1)
-    
-    def __cost(X, y, theta):
-        y1 = hypothesis(X, theta)
-        m = len(y1)
-        return sum(np.sqrt((y1-y)**2))/(2*m)
-    
-    def __gradientDescent(X, y, theta, alpha, epoch):
-        J = []
-        k = 0
-        while k < epoch:
-            y1 = hypothesis(X, theta)
-            for c in range(0, len(X.columns)):
-                theta[c] -= alpha*sum((y1-y)* X.iloc[:, c])/map(func, iter1)
-    
-    def fit(self):
+    def __standard(self, x, y):
+        """Train model with standard method"""
+        x = np.array(x)
+        y = np.matrix(y)
+        X = np.concatenate((np.ones(x.shape), x, x*x)).reshape(3, -1)
+        X = np.matrix(X)
+        X_T = X.T
+        x_dot_x = np.dot(X, X_T)
+        X_inv = np.linalg.inv(x_dot_x)
+        x_dot_y = np.dot(y, X_T)
+        self.__omega = np.dot(X_inv, x_dot_y.T)
+        
+    def __GradientDescent(self, x, y):
+        """Train model with Gradient Descent"""
+        # TODO
         pass
-    def predict(self):
+        
+    def predict(self, sample):
+        """Make prediction for provided sample"""
+        return self.__omega[0] + np.dot(self.__omega[1], sample) + np.dot(self.__omega[2], sample**2)
         pass
-    def plot(self):
+    def plot(self, test_data):
+        """plot a graph with test data"""
+        # TODO
         pass
-    def score(self):
+    
+    def print(self):
+        """prints some info"""
+        print(f"f(x) = {self.__omega[0]}*x^2 + {self.__omega[1]}x + {self.__omega[2]}")
+    
+    def score(self, sample):
         """Score of trained model"""
+        # TODO
         pass
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ Created on Fri May  5 17:31:43 2023
 import time
 import Data
 from RegressionTree import RegressionTree
+from PolynominalRegression import PolynominalRegression
 from copy import deepcopy
 datasets = [
     ['Data/CarPrice_Assignment.csv', 'price']
@@ -21,115 +22,26 @@ Data.save_to_latex(copy_data, 'Tables/CarPrice.tex')
 train_data, test_data = Data.split_data(data)
 
 y_train = train_data[y_label].to_numpy()
-x_train = train_data.drop(y_label, axis=1).to_numpy()
+x_train = train_data['horsepower'].to_numpy()
+#x_train = train_data.drop(y_label, axis=1).to_numpy()
 
 y_test = test_data[y_label].to_numpy()
-x_test = test_data.drop(y_label, axis=1).to_numpy()
+x_test = test_data['horsepower'].to_numpy()
+#x_test = test_data.drop(y_label, axis=1).to_numpy()
 
 
-y_train = train_data[y_label].to_numpy()
-x_train = (train_data.drop([y_label], axis=1) ).to_numpy()
+## test implementation
 
+regressors = [PolynominalRegression(), RegressionTree(min_sample_split=3, max_depth=3)]
 
-y_test = test_data[y_label].to_numpy()
-x_test = (test_data.drop([y_label], axis=1) ).to_numpy()
+for regressor in regressors:
+    time_0 = time.time()
+    
+    regressor.fit(x_train, y_train)
 
-
-# test implementation
-regressor = RegressionTree(min_sample_split=3, max_depth=3)
-
-time_0 = time.time()
-regressor.fit(x_train, y_train)
-print(f"time: {time.time() - time_0}\n")
-
-regressor.print_tree()
-
-#regressor.plot(x_test, y_test)
-
-
-### testy sklear
-
-import time
-import Data
-import matplotlib.pyplot as plt
-import numpy as np
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.tree import DecisionTreeRegressor
-
-data = Data.get_data(datasets[0][0])
-y_label = datasets[0][1]
-train_data, test_data = Data.split_data(data)
-
-# data
-y_train = train_data[y_label].to_numpy().reshape(-1,1)
-x_train = train_data['horsepower'].to_numpy().reshape(-1,1)
-
-y_test = test_data[y_label].to_numpy().reshape(-1,1)
-x_test = test_data['horsepower'].to_numpy().reshape(-1,1)
-
-
-## linear regression
-
-# regression
-regressor = LinearRegression()
-time_0 = time.time()
-regressor.fit(x_train, y_train)
-time_1 = time.time() - time_0
-
-# test
-y_pred = regressor.predict(x_test)
-plt.scatter(x_test, y_test, color='b')
-plt.plot(x_test, y_pred, color='k')
-plt.savefig('Plots/lin.png')
-plt.show()
-
-print("\nlinear regression:")
-print(regressor.intercept_, regressor.coef_)
-print(f"time: {time_1}")
-print(f"score: {regressor.score(x_test, y_test)}")
-
-## polynomial regression
-
-# regression
-time_0 = time.time()
-poly = PolynomialFeatures(degree=2, include_bias=False)
-poly_features = poly.fit_transform(x_train)
-regressor = LinearRegression()
-regressor.fit(poly_features, y_train)
-time_1 = time.time() - time_0
-
-# test
-x = poly.fit_transform(x_test)
-y_pred = regressor.predict(x)
-plt.scatter(x_test, y_test, color='b')
-plt.plot(x_test, y_pred, color='k')
-plt.savefig('Plots/poly.png')
-plt.show()
-
-print("\npolynominal regression:")
-print(regressor.intercept_, regressor.coef_)
-print(f"time: {time_1}")
-print(regressor.score(x, y_test))
-
-## regression tree
-
-# regression
-regressor = DecisionTreeRegressor(random_state = 0) 
-time_0 = time.time()
-regressor.fit(x_train, y_train)
-time_1 = time.time() - time_0
-
-# test
-y_pred = regressor.predict(x_test)
-X_grid = np.arange(min(x_test), max(x_test), 0.01)
-X_grid = X_grid.reshape((len(X_grid), 1))
-
-plt.scatter(x_test, y_test, color='red')
-plt.plot(X_grid, regressor.predict(X_grid), color='blue')
-plt.savefig('Plots/tree.png')
-plt.show()
-
-print("\ndecision tree regression:")
-print(f"time: {time_1}")
-print(f"score: {regressor.score(x_test, y_test)}")
+    print(f"time: {time.time() - time_0}\n")
+    if isinstance(regressor, RegressionTree):
+        regressor.print_tree()
+    else:
+        regressor.print()
+    #regressor.plot(x_test, y_test)
