84673db05658b1193d481e9b70b7a3108655adfe
Krzysztof Napiórkowski <krzysna@gmail.com>
2023-05-23 17:50:29 +0200

    minor fixes

--- Methods/Regressor.py
+++ Methods/Regressor.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 from abc import ABC, abstractmethod
 from sklearn.decomposition import PCA
+from copy import deepcopy
 import numpy as np
 import matplotlib.pyplot as plt
+
 
 class Regressor(ABC):
     name      = 'Regression'
@@ -32,22 +34,36 @@ class Regressor(ABC):
     def print(self):
         """prints some info"""
         pass
+    
+    def model(self):
+        return deepcopy(self)
 
-    def plot(self, x, y, transformed=False):
+    def best_featureset(self, features): # move from main
+        """Returns a featureset with best score"""
+        pass
+
+    def plot(self, x, y, transformed=False, x_train=None, y_train=None):
         """plot a graph with test data"""
         if transformed or x.ndim == 1 or x.shape[1] == 1:
             x_pred = np.arange(min(x), max(x), step=0.01)
             y_pred = [self.predict( [x for i in range(self._features)] ) for x in x_pred]
             plt.grid()
-            plt.scatter(x, y, color='b')
+            if x_train is not None and y_train is not None:
+                plt.scatter(x_train, y_train, color='r', label='train_data')
+            plt.scatter(x, y, color='b', label='test_data')
             plt.plot(x_pred, y_pred, color='green')
+            plt.legend()
         else:
             pca = PCA(n_components=2)
             test_data  = pca.fit_transform(x, y)
             x = test_data[:, 0]
             y = test_data[:, 1]
-            #self.fit(np.array(x).reshape(-1,1), y)
-            self.plot(x,y, transformed=True)
+            if x_train is not None and y_train is not None:
+                train_data = pca.fit_transform(x_train, y_train)
+                x_train = train_data[:, 0]
+                y_train = train_data[:, 1]
+            
+            self.plot(x,y, transformed=True, x_train=x_train, y_train=y_train)
     
     def score(self, x, y):
         """Score of trained model using coefficient of determination"""
@@ -60,5 +76,5 @@ class Regressor(ABC):
         SS_res = np.sum((y - y_pred)**2)
         SS_tot = np.sum((y - y_mean)**2)
         
-        return round(1 - (SS_res/SS_tot), 2)
+        return round(1 - (SS_res/SS_tot if SS_res/SS_tot < 1 else 1), 2)
         
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import time
 import Data
 import sys
 import matplotlib.pyplot as plt
+import numpy as np
+import itertools as itt
 
 # prep
 sys.path.append('Methods')
@@ -17,10 +19,34 @@ from PolynominalRegressor import PolynominalRegressor
 from LinearRegressor import LinearRegressor
 
 datasets = [
-    {'file_name': 'Student_Marks', 'y_label': 'Marks', 'x_labels': ['time_study']}, # score of a student according to hours spend studying
-    {'file_name': 'possum', 'y_label': 'site', 'x_labels': ['belly']}, # age of possum based on their features
-    {'file_name': 'CarPrice_Assignment', 'y_label': 'price',  'x_labels': ['horsepower', 'carlength', 'carwidth']}
-    ] # do znalezienia najlepszej cechy: [list(itt.combinations(CECHY, r=i)) for i in range(1,MAX_CECH+1)] TODO
+    {'file_name': 'Student_Marks', 'y_label': 'Marks', 'x_labels': ['time_study']},
+    {'file_name': 'CarPrice_Assignment', 'y_label': 'price',  'x_labels': ['horsepower', 'carlength', 'carwidth', 'peakrpm']}
+    ]
+def find_best_feature_combination(dataset, y_label, features, regressor):
+    featureList = [list(itt.combinations(features, r=i)) for i in range(1,len(features)+1)]
+    featureCombinations = list(itt.chain.from_iterable(featureList))
+    
+    y_train = dataset[y_label].to_numpy()
+    
+    best_model = None
+    best_score = -np.inf
+    best_featureSet = None
+    
+    for featureSet in featureCombinations:
+        featureSet = list(featureSet)
+        x_train = dataset.loc[:, featureSet].to_numpy()
+        regressor.fit(x_train, y_train)
+        score = regressor.score(x_train, y_train)
+        if score > best_score:
+            best_score = score
+            best_model = regressor.model()
+            best_featureSet = featureSet
+    
+    print(f"score_train: {best_score} - {best_featureSet}")
+    regressor = best_model
+    return best_featureSet
+
+
 for dataset in datasets:
     data = Data.get_data(f"{dataset['file_name']}.csv")
     #Data.inspect_data(data)
@@ -29,27 +55,32 @@ for dataset in datasets:
     train_data, test_data = Data.split_data(data)
 
     y_train = train_data[dataset['y_label']].to_numpy()
-    x_train = train_data.loc[:, dataset['x_labels']].to_numpy()
-    #x_train = train_data['horsepower'].to_numpy()
+    #x_train = train_data.loc[:, dataset['x_labels']].to_numpy()
     
     y_test = test_data[dataset['y_label']].to_numpy()
-    x_test = test_data.loc[:, dataset['x_labels']].to_numpy()
-    #x_test = test_data['horsepower'].to_numpy()
-    ## test implementation
+    #x_test = test_data.loc[:, dataset['x_labels']].to_numpy()
+    
+    
+    
     regressors = [LinearRegressor(), PolynominalRegressor(degree=3), DecisionTreeRegressor(min_sample_split=3, max_depth=4)]
     for regressor in regressors:
-        time_0 = time.time()
+        print(f"name: {regressor.name}")
+        featureSet = find_best_feature_combination(train_data, dataset['y_label'], dataset['x_labels'], regressor)
+        x_train = train_data.loc[:, featureSet].to_numpy()
+        time_0 = time.time() 
         regressor.fit(x_train, y_train)
         time_1 = time.time() - time_0
-        print(f"name: {regressor.name}")
-        print(f"time: {time_1}")
-        print(f"score_train: {regressor.score(x_train, y_train)}")
+        
+        # test data
+        x_test = test_data.loc[:, featureSet].to_numpy()
         print(f"score_test : {regressor.score(x_test, y_test)}")
-        #regressor.print()
+        print(f"time: {time_1}")
+        
+        regressor.print()
         print()
         
         if x_test.shape[1] != 1:
             regressor.fit_transform(x_train, y_train)
-        regressor.plot(x_test, y_test)
+        regressor.plot(x_test, y_test, x_train=x_train, y_train=y_train)
         plt.savefig(os.path.join('Plots',f"{dataset['file_name']}_{regressor.name}.png"))
         plt.show()
